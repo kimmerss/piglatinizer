b5db042c5ad442999bd92a43bf01aace0d06ce34
kimmerss <kimmerssvega@gmail.com>
2019-10-17 21:56:21 +0000

    barely did java

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+    $("button").click(function(){
+        let vowels =["a","e","i","o","u"];
+        let input= $("input").val();
 
 
 
-  
-  
-  
-  
+
+
+
+
+    });
 });
