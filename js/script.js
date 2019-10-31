f751862fc20a39ede5cc9a8a36cf1746a7ac2210
kimmerss <kimmerssvega@gmail.com>
2019-10-31 21:57:12 +0000

    added more code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
     $("button").click(function(){
-        let vowels =["a","e","i","o","u"];
-        let input= $("input").val();
+        var vowels =["a","e","i","o","u"];
+        let words= $("input").val();
+        let letters = words.split("")
+    $(".output").text(words);
+
+    if (vowels.includes(letters[0]){
+        $(".output").append(words+ "ay");
+
+    }
+
+
 
 
 
